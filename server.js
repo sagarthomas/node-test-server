51fa5dd870354ddf447c0b8f137d99d98a9b44e1
Sagar <sagarthomas27@hotmail.com>
2018-08-12 17:53:12 -0400

    Added projects page

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs', {
-        pageTitle: 'Site under maintenance'
-    });
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Site under maintenance'
+//     });
     
-});
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -45,6 +45,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About page',
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: "Projects",
+        welcomeMessage: 'Portfolio page'
     });
 });
 
